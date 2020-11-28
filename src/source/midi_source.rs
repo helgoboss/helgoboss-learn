1d649b318554215446f9ae1989f83f021edea44c
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-11-28 19:18:18 +0100

    Lower TODO priority

--- src/source/midi_source.rs
+++ src/source/midi_source.rs
@@ -414,7 +414,7 @@ impl MidiSource {
 
     /// Checks if this source consumes the given MIDI message. This is for sources whose events are
     /// composed of multiple MIDI messages, which is 14-bit CC and (N)RPN.
-    // TODO-medium Don't take ShortMessage by reference, never!
+    // TODO-low Don't take ShortMessage by reference, never!
     pub fn consumes(&self, msg: &impl ShortMessage) -> bool {
         use MidiSource::*;
         use StructuredShortMessage::*;
