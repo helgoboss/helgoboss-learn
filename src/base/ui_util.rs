bd40b4077984d8d06d4bdb73a66c9fe3d8fc07c1
Benjamin Klum <benjamin.klum@helgoboss.org>
2022-03-22 14:11:38 +0100

    Fix some clippy warnings related to large size difference between enum variants

--- src/base/ui_util.rs
+++ src/base/ui_util.rs
@@ -1,12 +1,6 @@
 pub fn format_percentage_without_unit(value: f64) -> String {
     let percentage = value * 100.0;
-    if (percentage - percentage.round()).abs() < 0.001 {
-        // No fraction. Omit zeros after dot.
-        format!("{:.4}", percentage)
-    } else {
-        // Has fraction. We want to display these.
-        format!("{:.4}", percentage)
-    }
+    format!("{:.4}", percentage)
 }
 
 pub fn parse_percentage_without_unit(text: &str) -> Result<f64, &'static str> {
